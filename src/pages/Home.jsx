6bfcfe7b2d3906478614a575d604958ba81b99f1
Atomu-Ishida <atomu.10.18@icloud.com>
2022-05-31 21:56:38 +0900

    finished recipe page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Popular from '../components/Popular';
 import Veggie from '../components/Veggie';
+import { motion } from 'framer-motion';
 
 function Home() {
   return (
-    <div>
-      <Popular />
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <Veggie />
-    </div>
+      <Popular />
+    </motion.div>
   );
 }
 
